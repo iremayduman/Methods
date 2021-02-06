b6842130b6829d92f6c4fdcd4c8e320628630307
iremcoder <77546520+iremcoder@users.noreply.github.com>
2021-02-06 22:03:12 +0300

    Metotlar

---
+++ Metotlar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyatı = 15;
+            string aciklama = "Amasya Elması";
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------");
+
+            }
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12,10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 12,9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuz", 12,8);
+
+
+        }
+    }
+}
